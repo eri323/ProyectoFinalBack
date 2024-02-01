628cda2517a0583e7a91fe30622e7367456c8499
ArianeVargas <ariane2fernanda4@gmail.com>
2024-02-01 14:12:50 -0500

    activar-desactivar lotes

--- controllers/Lote.js
+++ controllers/Lote.js
@@ -1,69 +1,79 @@
 import Lote from "../models/Lote.js";
 
 const httpLote = {
-    getLote: async (req, res) => {
-        try {
-            const lote = await Lote.find();
-            res.json({ lote });
-        } catch (error) {
-            res.status(400).json({ error });
-        }
-    },
-    getLoteId: async (req, res) => {
-        const { id } = req.params;
-        try {
-            const lote = await Lote.findById(id);
-            res.json({ lote });
-        } catch (error) {
-            res.status(400).json({ error });
-        }
-    },
-
-    postLote: async (req, res) => {
-        try {
-            const { Nombre, Presupuesto } = req.body;
-            const lote = new Lote({ Nombre, Presupuesto });
-            lote.save();
-            res.json({ lote });
-        } catch (error) {
-            res.status(400).json({ error });
-        }
-
-    },
-
-    putLote: async (req, res) => {
-        try {
-            const { id } = req.params;
-            const { Nombre, Presupuesto } = req.body;
-            const lote = await Lote.findByIdAndUpdate(id, { Nombre, Presupuesto }, { new: true });
-            res.json({ lote });
-        } catch (error) {
-            res.status(400).json({ error: "Error en el servidor" });
-        }
-    },
-
-
-    putLoteInactivar: async (req, res) => {
-        try {
-            const { id } = req.params;
-            const lote = await Lote.findByIdAndUpdate(id, { estado: 0 }, { new: true });
-            res.json({ lote });
-        } catch (error) {
-            res.status(400).json({ error });
-        }
-    },
-
-
-    putLoteActivar: async (req, res) => {
-        try {
-            const { id } = req.params;
-            const lote = await Lote.findByIdAndUpdate(id, { estado: 1 }, { new: true });
-            res.json({ lote });
-        } catch (error) {
-            res.status(400).json({ error });
-        }
+  getLote: async (req, res) => {
+    try {
+      const lote = await Lote.find();
+      res.json({ lote });
+    } catch (error) {
+      res.status(400).json({ error });
     }
+  },
+  getLoteId: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const lote = await Lote.findById(id);
+      res.json({ lote });
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  },
 
+  postLote: async (req, res) => {
+    try {
+      const { Nombre, Presupuesto } = req.body;
+      const lote = new Lote({ Nombre, Presupuesto });
+      lote.save();
+      res.json({ lote });
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  },
+
+  putLote: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { Nombre, Presupuesto } = req.body;
+      const lote = await Lote.findByIdAndUpdate(
+        id,
+        { Nombre, Presupuesto },
+        { new: true }
+      );
+      res.json({ lote });
+    } catch (error) {
+      res.status(400).json({ error: "Error en el servidor" });
+    }
+  },
+
+  putLoteInactivar: async (req, res) => {
+    console.log(req.params.id);
+    try {
+      const { id } = req.params;
+      const lote = await Lote.findByIdAndUpdate(
+        id,
+        { Estado: 0 },
+        { new: true }
+      );
+      res.json({ lote });
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  },
+
+  putLoteActivar: async (req, res) => {
+    console.log(req.params.id);
+    try {
+      const { id } = req.params;
+      const lote = await Lote.findByIdAndUpdate(
+        id,
+        { Estado: 1 },
+        { new: true }
+      );
+      res.json({ lote });
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  },
 };
 
 export default httpLote;
