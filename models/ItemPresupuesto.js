330726a774878c6f6112a20082207b8f0db9248e
eri323 <jomaldonado480@gmail.com>
2024-02-20 13:11:51 -0500

    erick

--- models/ItemPresupuesto.js
+++ models/ItemPresupuesto.js
@@ -1,8 +1,9 @@
+import { Decimal128 } from "mongodb";
 import mongoose from "mongoose";
 
 const ItemPresupuestoSchema = new mongoose.Schema({
     Nombre: { type: String, required: true },
-    Presupuesto: { type: Number, required: true},
+    Presupuesto: { type: Decimal128, required: true},
     Estado: { type: Number, default: 1 },
     createAT: { type: Date, default: Date.now },
 })
