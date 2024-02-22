0c095c926d6195414995d94d13b197e27e575a51
unknown <erickgutierrez085@gmail.com>
2024-02-22 11:32:12 -0500

    erick

---
+++ helpers/DistribucionPresupuesto.js
@@ -0,0 +1,23 @@
+import DistribucionPresupuesto from "../models/DistribucionPresupuesto.js.js";
+
+const helpersDisPresupuesto = {
+    validarDisPreUnica: async ( idItem, req) => {
+        try {
+            const idLote = req.req.body.idLote;
+
+            const existe = await DistribucionPresupuesto.findOne({ 
+                idItem: idItem,
+                idLote: idLote,
+            });
+        
+            if (existe) {
+                throw new Error("Esta distribucion ya existe");
+            } 
+        } catch (error) {
+            throw new Error(error)
+        } 
+    },
+};
+
+
+export default helpersDisPresupuesto;
